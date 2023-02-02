f65fa62d0969687c76af60df205ec5078d7f3541
NOTAPC\NotAUser2000 <sewardwoden@gmail.com>
2023-02-02 16:05:44 +0600

    Demonstration component fixes

--- components/for_pages/main/Demonstration/index.tsx
+++ components/for_pages/main/Demonstration/index.tsx
@@ -43,53 +43,59 @@ export default function Demonstration({ isActive, onCancel }: Props) {
       }}
     >
       <div className={styles.root} ref={nodeRef}>
-        <Image className=
-          {styles.img} src={'/img/logos/screen.svg'} alt='' fill />
+        <div className={styles.screen}><Image className=
+          {styles.img} src={'/img/logos/screen.svg'} alt='' fill /></div>
         <div className={styles.container}>
-          <div className={styles.top}>
-            <div className={styles.left}>
-              <div className={styles.text}>
-                для демонстрации подключитесь к wifi
+          <div className={styles.wrapper}>
+            <div className={styles.top}>
+              <div className={styles.withQr}>
+                <div className={styles.left}>
+                  <div className={styles.text}>
+                    для демонстрации<br/> подключитесь к wifi
+                  </div>
+                  <div className={styles.field}>
+                    <span>сеть</span><span>name</span>
+                  </div>
+                  <div className={styles.field}>
+                    <span>пароль</span><span>D D M E J N</span>
+                  </div>
+                </div>
+                <div className={styles.right}>
+                  <Image className={styles.qr} src={'/img/dev/qr-jesus.png'} alt='' fill />
+                </div>
               </div>
-              <div className={styles.field}>
-                <span>сеть</span><span>name</span>
-              </div>
-              <div className={styles.field}>
-                <span>пароль</span><span>D D M E J N</span>
+              <div className={styles.devices}>
+                <div className={styles.use}>
+                  использовать устройство
+                </div>
+                <div className={styles.list}>
+                  <div onClick={() => setDevice('apple')}
+                    className={classNames(styles.device, { [styles.active]: device === 'apple' })}>
+                    apple
+                  </div>
+                  <div onClick={() => setDevice('win')}
+                    className={classNames(styles.device, { [styles.active]: device === 'win' })}>
+                    windows
+                  </div>
+                  <div onClick={() => setDevice('android')}
+                    className={classNames(styles.device, { [styles.active]: device === 'android' })}>
+                    android
+                  </div>
+                </div>
               </div>
             </div>
-            <div className={styles.right}>
-              <Image className={styles.qr} src={'/img/dev/qr-jesus.png'} alt='' fill />
+            <div className={styles.imgWrapper}>
+              <Image className={styles.instruction} src={getImg(device)} alt='' fill />
+              {device !== 'win' ?
+                <div className={styles.tip}>
+                  * room name—переговорная 1
+                </div>
+                : null}
             </div>
           </div>
-          <div className={styles.devices}>
-            <div className={styles.use}>
-              использовать устройство
-            </div>
-            <div className={styles.list}>
-              <div onClick={() => setDevice('apple')}
-                className={classNames(styles.device, { [styles.active]: device === 'apple' })}>
-                apple
-              </div>
-              <div onClick={() => setDevice('win')}
-                className={classNames(styles.device, { [styles.active]: device === 'win' })}>
-                windows
-              </div>
-              <div onClick={() => setDevice('android')}
-                className={classNames(styles.device, { [styles.active]: device === 'android' })}>
-                android
-              </div>
-            </div>
+          <div className={styles.title} onClick={onCancel}>
+            завершить демонстрацию
           </div>
-          <Image className={styles.instruction} src={getImg(device)} alt='' fill />
-          {device !== 'win' ?
-            <div className={styles.tip}>
-              * room name—переговорная 1
-            </div>
-            : null}
-        </div>
-        <div className={styles.title} onClick={onCancel}>
-          завершить демонстрацию
         </div>
       </div>
     </CSSTransition >
