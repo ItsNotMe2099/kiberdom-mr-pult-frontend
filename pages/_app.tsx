ff8907ad807957a46a9df370fb6a80623539ecea
NOTAPC\NotAUser2000 <sewardwoden@gmail.com>
2023-02-14 15:07:12 +0600

    meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,8 @@ import 'styles/globals.scss'
 import type { AppProps as NextAppProps } from 'next/app'
 import { AppWrapper } from 'context/state'
 import Snackbar from 'components/for_pages/Layout/Snackbar'
-import { useEffect } from 'react'
+import Head from 'next/head'
+//import { useEffect } from 'react'
 
 export interface AppProps extends NextAppProps {
   pageProps: {
@@ -14,18 +15,23 @@ export interface AppProps extends NextAppProps {
 function MyApp({ Component, pageProps }: AppProps) {
 
 
-  useEffect(() => {
+  /*useEffect(() => {
     if (typeof window !== 'undefined') {
       alert(`${window.innerWidth} window.innerWidth, ${window.innerHeight} window.innerHeight, ${window.outerWidth} window.outerWidth, ${window.outerHeight} indow.outerHeight`)
     }
-  })
+  })*/
 
 
   return (
-    <AppWrapper>
-      <Component {...pageProps} />
-      <Snackbar />
-    </AppWrapper>
+    <>
+      <Head>
+        <meta content='width=device-width' initial-scale={1.0} maximum-scale={1.0} user-scalable={0} name='viewport' />
+      </Head>
+      <AppWrapper>
+        <Component {...pageProps} />
+        <Snackbar />
+      </AppWrapper>
+    </>
   )
 }
 
